c296014d152312e52d6b90730354cc6019a16121
qkrxodud00 <qkrxodud00@gmail.com>
2022-03-25 19:58:18 +0900

    *init jpabook

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); // 애플리케이션 에서 한개만 만들어 져야된다.
+
+        EntityManager em = emf.createEntityManager(); //하나의 단위를 만들때마다 만들어 줘야된다.
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+
+}
